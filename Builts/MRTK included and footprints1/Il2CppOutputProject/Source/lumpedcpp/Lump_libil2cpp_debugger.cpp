214d656b13473aff7bd10a05833b325d43e3a725
Philipp <34287258+Phippe@users.noreply.github.com>
2022-11-17 16:24:14 +0100

    Remove builTs folder xD

--- Builts/MRTK included and footprints1/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_debugger.cpp
+++
@@ -1,2 +0,0 @@
-#include "il2cpp-config.h"
-#include "D:\DOCUMENTOS\MASTER\1 semestre\Mixed reality\github\Mixed-Rehabilitation\Builts\MRTK included and footprints1\Il2CppOutputProject\IL2CPP\libil2cpp\debugger\il2cpp-stubs.cpp"
--- Builts/MRTK included and footprints1/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_vm-utils.cpp
+++
@@ -1,8 +0,0 @@
-#include "il2cpp-config.h"
-#include "D:\DOCUMENTOS\MASTER\1 semestre\Mixed reality\github\Mixed-Rehabilitation\Builts\MRTK included and footprints1\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\BlobReader.cpp"
-#include "D:\DOCUMENTOS\MASTER\1 semestre\Mixed reality\github\Mixed-Rehabilitation\Builts\MRTK included and footprints1\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\Debugger.cpp"
-#include "D:\DOCUMENTOS\MASTER\1 semestre\Mixed reality\github\Mixed-Rehabilitation\Builts\MRTK included and footprints1\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\NativeDelegateMethodCache.cpp"
-#include "D:\DOCUMENTOS\MASTER\1 semestre\Mixed reality\github\Mixed-Rehabilitation\Builts\MRTK included and footprints1\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\NativeSymbol.cpp"
-#include "D:\DOCUMENTOS\MASTER\1 semestre\Mixed reality\github\Mixed-Rehabilitation\Builts\MRTK included and footprints1\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\VmStringUtils.cpp"
-#include "D:\DOCUMENTOS\MASTER\1 semestre\Mixed reality\github\Mixed-Rehabilitation\Builts\MRTK included and footprints1\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\icalls\mscorlib\System.Threading\Interlocked.cpp"
-#include "D:\DOCUMENTOS\MASTER\1 semestre\Mixed reality\github\Mixed-Rehabilitation\Builts\MRTK included and footprints1\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\icalls\mscorlib\System\Math.cpp"
