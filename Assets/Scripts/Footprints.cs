469ce83404620a191a9fef6208ce489dbeaae016
BlancaGutiez <blanca.gutiez.bravo@alumnos.upm.es>
2022-12-05 20:59:32 +0100

    new footprints shape and finetunnings
    - Footprint shape improved
    - First footprint is now correctly rotated
    - CPU menu is not shown anymore
    - Line now doesn't rotate (still a little bit)
    - Modified speed and acceleration of
      navmeshagent
    - Footprints a little bit lower

--- Assets/Scripts/Footprints.cs
+++ Assets/Scripts/Footprints.cs
@@ -18,10 +18,21 @@ public class Footprints : MonoBehaviour
     // Number of footprints to keep.
     public int maxFootprints = 10;
 
+    // Right Footprint prefab
+    public GameObject rightFootprintPrefab;
+    
+    // Left Footprint prefab
+    public GameObject leftFootprintPrefab;
+
     // The list of footprints.
     private List<GameObject> footprints;
     private bool isPlayerCreated = false;
     public LineRenderer lineRenderer;
+    
+    // Gait parameters 
+    public float stepLength = 0.2f;
+    public float stepWidth = 0.4f;
+    private bool isFirstFootprint = true;
 
 
     // Start is called before the first frame update
@@ -29,8 +40,8 @@ public class Footprints : MonoBehaviour
     {
         // Initialize the list of footprints.
         footprints = new List<GameObject>();
-
-
+        
+        //lineRenderer.alignment = LineAlignment.Local;
 
     }
 
@@ -53,6 +64,7 @@ public class Footprints : MonoBehaviour
         {
             Destroy(footprint);
         }
+        isFirstFootprint = true;
     }
     private bool leftFoot = true;
 
@@ -61,10 +73,14 @@ public class Footprints : MonoBehaviour
     {
         if (player != null && isPlayerCreated && player.transform.position != Camera.main.transform.position)
         {
-
+            
             // Get the player's current position.
             currentPosition = player.transform.position;
-
+            if (isFirstFootprint)
+            {
+                isFirstFootprint = false;
+                lastPositionFootprint = currentPosition;
+            }
             // PRINT LINE
             // If the player has moved more than 0.05 units since the last update add a new point to the linerenderer.
             if (Vector3.Distance(currentPosition, lastPositionLine) > 0.05f)
@@ -78,31 +94,56 @@ public class Footprints : MonoBehaviour
             }
 
             // PRINT FOOTPRINTS If the player has moved at least 0.5 units (estimated step size) since the last frame, create a new footprint.
-            if (Vector3.Distance(currentPosition, lastPositionFootprint) > 0.5f)
+            if (Vector3.Distance(currentPosition, lastPositionFootprint) > stepLength)
             {
+                GameObject footprint = null;    
                 // Create a new footprint.
-                GameObject footprint = GameObject.CreatePrimitive(PrimitiveType.Cube);
-
-                // Set the footprint's position to the player's current position, but with a y offset of -0.5.
-                footprint.transform.position = new Vector3(currentPosition.x, currentPosition.y + 0.02f, currentPosition.z);
-
-                // Alternate between having the footprint be slightly to the left or right of the player's trajectory.
-                Vector3 trajectory = currentPosition - lastPositionFootprint;
                 if (leftFoot)
                 {
-                    footprint.transform.position += new Vector3(trajectory.z, 0, -trajectory.x) * 0.1f;
+                    footprint = GameObject.Instantiate(leftFootprintPrefab);         
                 }
                 else
                 {
-                    footprint.transform.position += new Vector3(-trajectory.z, 0, trajectory.x) * 0.1f;
+                    footprint = GameObject.Instantiate(rightFootprintPrefab);
+                }
+
+                // Set the footprint's position to the player's current position, but with a y offset of -0.5.
+                footprint.transform.position = new Vector3(currentPosition.x, currentPosition.y + 0.0001f, currentPosition.z);
+
+                // Alternate between having the footprint be 0.5f to the left or right of the player's trajectory.
+                
+                Vector3 trajectory = currentPosition - lastPositionFootprint;
+                float angle = Mathf.Atan2(trajectory.z, trajectory.x);// * Mathf.Rad2Deg;
+
+            
+
+
+                if (leftFoot)
+                {
+                    footprint.transform.position += new Vector3(-(stepWidth/2)*Mathf.Sin(angle), 0, (stepWidth / 2) * Mathf.Cos(angle));
+                    //footprint.transform.position += new Vector3(trajectory.z, 0, -trajectory.x) * 0.1f;
+                }
+                else
+                {
+                    footprint.transform.position += new Vector3((stepWidth / 2) * Mathf.Sin(angle), 0, -(stepWidth / 2) * Mathf.Cos(angle));
+                    //footprint.transform.position += new Vector3(-trajectory.z, 0, trajectory.x) * 0.1f;
                 }
                 leftFoot = !leftFoot;
 
                 // Set the footprint's scale.
-                footprint.transform.localScale = new Vector3(0.3f, 0.01f, 0.3f);
+                //footprint.transform.localScale = new Vector3(0.3f, 0.01f, 0.3f);
+
+                //Set the footprint's rotation.
+                // calculate the rotation from 0 to the current position
+                //footprint.transform.rotation = Quaternion.LookRotation(currentPosition);
+                //angle between two vectors
+                
+                //float rotationAngle = Vector3.Angle(new Vector3(1, 0, 0), currentPosition);
+                footprint.transform.rotation = Quaternion.Euler(0, -angle * Mathf.Rad2Deg+90, 0);
+                //footprint.transform.Rotate(currentPosition);
 
                 // Set the footprint's color.
-                footprint.GetComponent<Renderer>().material.color = Color.black;
+                //footprint.GetComponent<Renderer>().material.color = Color.black;
 
                 // Add the footprint to the list of footprints.
                 footprints.Add(footprint);
